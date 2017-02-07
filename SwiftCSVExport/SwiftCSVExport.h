adf01e37609591f8bce18e1e7938cbf25be70d45
Vignesh Kumar <vigneshkumar@qburst.com>
2017-02-07 14:40:59 +0530

    Fixed the pod version issue.

--- SwiftCSVExport/SwiftCSVExport.h
+++ SwiftCSVExport/SwiftCSVExport.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 vigneshuvi. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftCSVExport.
 FOUNDATION_EXPORT double SwiftCSVExportVersionNumber;
