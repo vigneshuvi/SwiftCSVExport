1fad077be9d1d23943b76ffbf39e77ae9a06bf03
Cihat Gündüz <cg@papershift.com>
2021-02-01 17:05:47 +0100

    Fix issue with Xcode 12.4 & remove duplicate files
    Workaround for https://bugs.swift.org/browse/SR-13579

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftCSVExport",
-    platforms: [.macOS(.v10_10), .iOS(.v8)],
+    platforms: [.macOS(.v10_10), .iOS(.v9)],
     products: [.library(name: "SwiftCSVExport", targets: ["SwiftCSVExport"])],
     targets: [
         .target(
             name: "SwiftCSVExport",
-            path: "Sources"
+            path: "SwiftCSVExport/Sources"
         )
     ]
 )
